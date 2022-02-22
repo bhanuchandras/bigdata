4e8e553102a3de5613cedeb03889527090ef95fe
root <root@hadoop-master.us-central1-a.c.maximal-beach-340105.internal>
2022-02-22 06:15:53 +0000

    -

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 spark-submit --master yarn  file_cl.py
 
 sudo -u hdfs hdfs dfs -chmod 777  /user
-
+sudo -u hdfs hdfs dfs -mkdir /data
 sudo -u hdfs hdfs dfs -chmod 777  /data
 
+sudo su
+cd /var/log/
+hdfs dfs -put cloudera-scm-server /data
+hdfs dfs -put spark /data
+hdfs dfs -put hadoop-hdfs /data
--- file_cl.py
+++ file_cl.py
@@ -1,9 +1,13 @@
 import pyspark
 from pyspark import SparkConf
 from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
 conf = SparkConf()
 conf.setAppName('spark-yarn-bhanu')
 sc = SparkContext(conf=conf)
+spark = SparkSession.builder.appName('spark-yarn-bhanu').getOrCreate()
+
 #cols=["path","isDirectory","length","replication","blocksize","modification_time","access_time","owner","group","permission","isSymlink","hasAcl","isEncrypted","isErasureCoded"]
 fl=open("list_files.csv",'w')
 #sc = pyspark.SparkContext('local[*]')
@@ -11,7 +15,7 @@ hadoop = sc._jvm.org.apache.hadoop
 fs = hadoop.fs.FileSystem
 conf = hadoop.conf.Configuration()
 path = hadoop.fs.Path('/data')
-
+ll=[]
 def traverse_dir(path):
     for f in fs.get(conf).listStatus(path):
         #print(f.toString())
@@ -20,12 +24,14 @@ def traverse_dir(path):
         for i in f.toString().split(";"):
             #print(str(i).rstrip('}').replace('HdfsNamedFileStatus{',''))
             v[str(i).rstrip('}').replace('HdfsNamedFileStatus{','').split('=')[0].strip()] = str(i).rstrip('}').replace('HdfsNamedFileStatus{','').split('=')[1]
-        fl.write(f.toString()+"\n")
-        print(v)
+        #print(v)
+	ll.append(v)
         if f.isDirectory():
             traverse_dir(f.getPath())
 traverse_dir(path)
-fl.close()
+print(ll)
+dataframe = spark.createDataFrame(ll)
+dataframe.show()
 #for f in fs.get(conf).listStatus(path):
     #print(f.getPath(), f.getLen())
 #    print(f.toString())
