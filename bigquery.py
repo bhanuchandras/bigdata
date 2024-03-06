4d53f9049dce1a7f8afd37b51d6cf9a9cc41e0ba
Bhanu <bhanuandchandra@gmail.com>
2024-03-06 18:29:25 +0530

    Update bigquery.py

--- bigquery.py
+++ bigquery.py
@@ -9,13 +9,24 @@ credentials, project_id = google.auth.default()
 client = bigquery.Client(credentials=credentials, project=project_id)
 
 # Specify the public dataset and table (replace with actual names)
-dataset_id = "public_dataset"
-table_id = "google_trends.international"
+dataset_id = "bigquery-public-data"
+table_id = "google_trends.top_terms"
 
 # Download the data to a temporary file (replace with desired format)
 query = f"""
-SELECT *
-FROM `{dataset_id}.{table_id}`
+SELECT
+   refresh_date AS Day,
+   term AS Top_Term,
+-- These search terms are in the top 25 in the US each day.
+   rank,
+FROM `bigquery-public-data.google_trends.top_terms`
+WHERE
+rank <= 10
+-- Choose only the top term each day.
+AND refresh_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 WEEK)
+-- Filter to the last 2 weeks.
+GROUP BY Day, Top_Term, rank
+ORDER BY Day DESC
 """
 destination_file = "local_data.csv"  # Replace with desired filename and format
 
